4b0ac111a99b0e8397181c59f2427d87c69e45b4
AnshOz <ansh_oswal@mymail.sutd.edu.sg>
2024-10-17 00:23:34 +0800

    initialised mongo db atlas

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const userRoutes = require('./routes/users');
 
 const app = express();
@@ -15,8 +16,17 @@ app.use((req, res, next) => {
 //Routs
 app.use('/api/users', userRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Server is listening on port 3000');
-});
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+         // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Server is listening on port 3000');
+        });
+  
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 
